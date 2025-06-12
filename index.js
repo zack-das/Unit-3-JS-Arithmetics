82f51ca59884778d9874495ad8d03e011a120532
Zack Das <izackdas@gmail.com>
2025-06-12 08:43:22 +0300

    index.js update

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
+
+const random = () => Math.floor(Math.random() * 100) + 1;
+const mod = (num3, num4) => num3 % num4;
+const max = Math.max(5, 10, 15, 20);
